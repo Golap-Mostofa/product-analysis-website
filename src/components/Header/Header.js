7f29dd4e7d74a6ec563a8cf54a56fe6705c4bd68
Golap-mostofa <mostofa6410@gmail.com>
2022-04-03 20:42:57 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-lime-700 p-6 text-right'>
+            <a className='mr-3 text-yellow-100 text-100' href="">Home</a>
+            <a className='mr-3 text-yellow-100 text-100' href="">Home</a>
+            <a className='mr-3 text-yellow-100 text-100' href="">about</a>
+            <a className='mr-3 text-yellow-100 text-100' href="">product</a>
+        </div>
+    );
+};
+
+export default Header;
