9827f9b9c6e4d3344ddd33c559be0fdc287b368b
Golap-mostofa <mostofa6410@gmail.com>
2022-04-05 12:50:31 +0600

    update rechcate

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>about </h2>
+        <h2 className='text-indigo-900 text-3xl mt-5 font-bold'>Hello i am Font-End developer in new 2022</h2>
         </div>
     );
 };
